3d5b7949abd9843b6cd4bf53853cadbc076473c3
Palash Bauri <palashbauri1@gmail.com>
2022-12-22 12:06:56 +0530

    added basic dinolog example file

---
+++ dinolog.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+var logfile = "example.dinolog.txt"
+
+func main(){
+	data , err := os.ReadFile(logfile)
+
+	if err != nil{
+		log.Fatalln("Failed to read dinolog file ->" + logfile)
+	}
+
+	fmt.Printf("[+] dinolog file ==>\n\n%s<== file end" , string(data))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module dinolog
+
+go 1.19
